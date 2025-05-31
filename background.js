64c140e54adcfce6e02e25cc20fde33d90739ba9
Xi Lu <448153639@qq.com>
2025-05-31 10:28:01 -0400

    remove context menu

--- background.js
+++ background.js
@@ -7,24 +7,3 @@ chrome.runtime.onInstalled.addListener(() => {
   });
   chrome.storage.sync.set({ blockedClasses: [] });
 });
-
-// Handle context menu clicks
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "blockClass") {
-    // Send message to content script to get the class name
-    chrome.tabs.sendMessage(tab.id, { action: "getClassName" }, (response) => {
-      if (response && response.classNames) {
-        // Add the classes to blocked classes
-        chrome.storage.sync.get(['blockedClasses'], (result) => {
-          const classes = result.blockedClasses || [];
-          response.classNames.forEach(className => {
-            if (!classes.includes(className)) {
-              classes.push(className);
-            }
-          });
-          chrome.storage.sync.set({ blockedClasses: classes });
-        });
-      }
-    });
-  }
-}); 
